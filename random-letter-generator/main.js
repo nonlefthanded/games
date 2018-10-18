30602130c76c4e2ae1baeaeb259f92f4c8725738
CJ Stritzel <cjstritzel@Admins-MacBook-Pro.local>
2018-10-18 16:56:38 -0700

    adding the random number generator

--- random-letter-generator/main.js
+++ random-letter-generator/main.js
@@ -10,10 +10,8 @@ $(function() {
   }
   for (i=0; i<_.rolls;i++){
     setTimeout(function(i){
-      //console.log(i);
       _.random.number = Math.floor(Math.random() * 25);
       _.random.letter = _.alphabet[_.random.number];
-      // console.log(_.random.number + ' - ' + _.random.letter);
       $('#letter h1').html(_.random.letter);
     },i*_.timeBetween,i);
   }
