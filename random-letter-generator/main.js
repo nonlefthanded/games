1e13a1db97877b00808801b3ba7f5eae5b6f9fe6
CJ Stritzel <cjstritzel@Admins-MacBook-Pro.local>
2018-10-18 16:50:14 -0700

    adding the random number generator

---
+++ random-letter-generator/main.js
@@ -0,0 +1,21 @@
+$(function() {
+  var _ = {
+    'alphabet' : [],
+    'timeBetween' : 40,
+    'random' : {'number':0,'letter':'A'}
+  };
+  for (i = 65; i < 91; i++) {
+    _.alphabet.push(String.fromCharCode(i));
+  }
+  for (i=0; i<100;i++){
+    setTimeout(function(i){
+      //console.log(i);
+      _.random.number = Math.floor(Math.random() * 25);
+      _.random.letter = _.alphabet[_.random.number];
+      // console.log(_.random.number + ' - ' + _.random.letter);
+      $('#letter h1').html(_.random.letter);
+    },i*_.timeBetween,i);
+
+  }
+  // console.log(_);
+})
